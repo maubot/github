d14ccf52251e37a245fb4a36169a5ee6bac2044c
Alexey Minnekhanov <alexey.min@gmail.com>
2025-04-08 13:34:56 +0300

    Ignore push to personal refs "username/feature"
    Signed-off-by: Alexey Minnekhanov <alexey.min@gmail.com>

--- github/template/util.py
+++ github/template/util.py
@@ -55,6 +55,13 @@ class TemplateUtil:
         return ref.split("/", 2)[2]
 
     @staticmethod
+    def ref_is_personal(ref: str) -> bool:
+        # Detect personal branches
+        # return True if branch name contains '/', like in
+        #     "refs/heads/username/some-branch"
+        return '/' in ref.split("/", 2)[2]
+
+    @staticmethod
     def join_human_list(data: List[str], *, joiner: str = ", ", final_joiner: str = " and ",
                         mutate: Callable[[str], str] = lambda val: val) -> str:
         if not data:
